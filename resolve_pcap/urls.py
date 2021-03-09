3d82a475810819e8d7c4ad4abdab49c3ccd7987e
fengchuan <1299693999@qq.com>
2021-03-09 16:48:49 +0800

    fisrtcommit

--- resolve_pcap/urls.py
+++ resolve_pcap/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import resove_pcapfile
+from django.views.generic import TemplateView
 urlpatterns = [
+path('',TemplateView.as_view(template_name='index.html')),
     path('api/uploadpcapfile/', resove_pcapfile),
 ]
